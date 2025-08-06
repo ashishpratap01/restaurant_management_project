edf2a7f2110d1303653a2d29fb3268fc2818c313
Ashish Pratap <139653805+ashishpratap01@users.noreply.github.com>
2025-08-06 19:31:31 +0530

    20884

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import users
+
+class UserProfile(models.model):
+    user = models.OneTOOneField(user,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    def __str__(self):
+        return self.user.username
 
 # Create your models here.
